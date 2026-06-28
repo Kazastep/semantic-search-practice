301bd64842ccf5721729cf48d4abe2f851b0c3c7
Stepan <stepan.kazakov@gmail.com>
2026-06-28 16:01:50 +0300

    Finish 3 stage

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import pandas 
 
 with open("dataset/code_corpus.json", encoding="utf-8") as f:
     corpus = json.load(f)
@@ -13,13 +14,3 @@ print(len(corpus))      # 200
 print(len(questions))   # 25
 print(len(categories))  # 5
 
-func = corpus[0]
-print(func["id"])             # func_001
-print(func["language"])       # python
-print(func["function_name"])  # verify_jwt_token
-print(func["category"])       # auth
-
-q = questions[0]
-print(q["question_id"])       # q_01
-print(q["query"])             # как проверить, истёк ли токен?
-print(q["correct_chunk_id"])  # func_001
